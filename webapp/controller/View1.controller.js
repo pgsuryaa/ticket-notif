03e9d70a9bd915f65ab49d76016f4e6db73dde0d
Suriya Pandiyan <imsuryapandiyan@gmail.com>
2023-10-16 15:56:30 +0000

    Init

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,53 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("leo.controller.View1", {
+            onInit: function () {
+
+            },
+
+            onAfterRendering: function () {
+
+                setInterval(function () {
+                    $.ajax({
+                        url: "/v3/movies/search/movie?meta=1&reqData=1&city=chennai&movieCode=rur_1kciu&version=3&site_id=1&channel=web&child_site_id=1",
+                        cache: false,
+                        success: function (oData) {
+                            console.log(oData)
+                            if (!this._iCinemaCount) {
+                                this._iCinemaCount = oData.data.cinemasOrder.length
+                            }
+                            else {
+                                if (this._iCinemaCount < oData.data.cinemasOrder.length) {
+                                    this._iCinemaCount = oData.data.cinemasOrder.length
+                                    this.sendMessage()
+                                }
+                            }
+                        }.bind(this)
+                    });
+                }.bind(this), 10 * 1000); // 60 * 1000 milsec
+            },
+
+            sendMessage: function () {
+                $.ajax({
+                    type: 'POST',
+                    url: "/2010-04-01/Accounts/AC068bd4d8856f944d4a6d3e1694e18d78/Messages.json",
+                    async: true,
+                    data: {
+                        "To": "+919047957469",
+                        "From": "+13869688349",
+                        "Body": "New cinemas found in Paytm"
+                    }
+                }).done(function (results) {
+                    console.log(results);
+                });
+            }
+
+        });
+    });
